3fec2aa125ad3f581fbc28846c37df5e1a15c74d
J0KER <dionatsir@gmail.com>
2019-02-01 09:56:07 +0700

    new

---
+++ src/components/user/UserEdit.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h3>Edit the User</h3>
+        <p>Locale: {{ $route.query.locale }} </p>
+        <p>Analytics: {{ $route.query.q }} </p>
+        <hr>
+        <button class="btn btn-danger" @click="confirmed = true">Confirm</button>
+        <div style="height: 900px"></div>
+        <p id="data">Some extra text</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            confirmed: false
+        }
+    },
+    beforeRouteLeave(to, from, next) {
+        if (this.confirmed) {
+            next();
+        } else {
+            if (confirm('Are you sure?')) {
+                next();
+            } else {
+                next(false);
+            }
+        }
+    }
+}
+</script>
